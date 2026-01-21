f8cc7f6eb2653a79b9a61c62cbb6a54e88551f5e
Stella <s.wangyj06@gmail.com>
2026-01-21 18:39:12 -0500

    syuncing mac and win?

---
+++ guandan/shuffle.py
@@ -0,0 +1,15 @@
+import math
+import random
+from cards import *  
+
+class Player:
+    def __init__(self, name):
+        self.hand = []
+        self.turn = False
+        self.name = name
+        
+
+    def play_card(self):
+        return
+    
+
