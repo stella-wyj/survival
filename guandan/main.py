f83e8457a5fafb40e6bcb61c3db568e9b8b61a36
stella-wyj <s.wangyj06@gmail.com>
2026-01-23 15:16:19 -0500

    testing the deal method, now deals to all players

--- guandan/main.py
+++ guandan/main.py
@@ -6,3 +6,8 @@ my_room = Room()
 my_deck = Deck(2)
 
 Deck.deal(my_deck.deck, my_room.room)
+for player in my_room.room:
+    print(player.name)
+    for card in player.hand:
+        print(card.suit, card.rank)
+        
