ee61a54898da0429d132c8a7959b76e107b8df3c
Stella <s.wangyj06@gmail.com>
2026-01-25 21:35:18 -0500

    Implemented some game logic and trump/wild card
    Added the trump card logic to the game. Now when you deal that cards the
    trump card in hearts will be the wild card. Some game logic implemented
    as well.

--- guandan/cards.py
+++ guandan/cards.py
@@ -1,14 +1,17 @@
 import math
 import random
-from player import *
+from room import *
+from game import *
 
 NUM_CARDS=52
 NUM_PLAYERS=4
+MAX_THROW = 6 # max number of cards you can play => EXCEPTION BOMBS (8 CARDS PLAYED)
 
 class Card:
-    def __init__(self, suit, rank):
+    def __init__(self, suit, rank, wild_card=False):
         self.suit = suit
         self.rank = rank
+        self.wild_card = wild_card
 
     def get_suit(self):
         return self.suit
@@ -17,28 +20,50 @@ class Card:
         return self.rank
     
 class Deck:
-    def __init__(self, num_decks):
+    def __init__(self, num_decks, trump):
         self.deck = []
         self.num_decks = num_decks
         suits = ["D", "C", "H", "S"]
+
+        # create new deck each time trump suit change 
         for i in range(NUM_CARDS):
-            self.deck.extend([Card(suits[math.floor(i//13)], (i%13+1))]*num_decks)    
+            # if the number created is a trump number, ensure that self.wild_card is true
+            if (i%13+1 == trump and suits[math.floor(i//13)]=="H"):
+                self.deck.extend([Card(suits[math.floor(i//13)], (i%13+1), True)]*num_decks)
+            else:
+                self.deck.extend([Card(suits[math.floor(i//13)], (i%13+1))]*num_decks)
         self.deck.extend([Card("SJ", 14)]*num_decks) # small joker
         self.deck.extend([Card("BJ", 15)]*num_decks) # big joker
+
+        # label the wild cards in the deck, as well as trump number
+
+
     
     def deal(deck, players):
         random.shuffle(deck)
         random.shuffle(deck)
         for i in range(len(deck)):
             players[math.floor(i%NUM_PLAYERS)-1].hand.append(deck[i])
-            
+
+        # sort by power 
+        for player in players:
+            player.hand.sort(key=Card.get_rank)
+
+        # logic for playing combos
+        
+
+
+
+
 
 class Combos:
+    COMBO_ORDER = {"single":1, "double":1, "triple": 1, "straight": 1, "tubes": 1, "full_house": 1, "plates":1,
+                   "4_bomb":2, "5_bomb": 3, "6_bomb": 4, "7_bomb": 5, "8_bomb": 6} # tubes are double striaghts and plates are triple-triple
+
+
     def __init__(self):
         pass
 
 
 
-# testingggg
-my_deck = Deck(2)
 
--- guandan/main.py
+++ guandan/main.py
@@ -1,13 +1,15 @@
 ## Deal Cards
-from player import *
+from room import *
 from cards import *
+from game import *
 
 my_room = Room()
-my_deck = Deck(2)
+my_game = Game()
+my_deck = Deck(2, my_game.get_trump())
 
 Deck.deal(my_deck.deck, my_room.room)
 for player in my_room.room:
-    print(player.name)
+    print("---------- Player", player.name, "----------")
     for card in player.hand:
-        print(card.suit, card.rank)
+        print(card.suit, card.rank, card.wild_card)
         
