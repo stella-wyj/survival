4695495e00fc146ebc1e0716f87917d47a9efaeb
stella-wyj <s.wangyj06@gmail.com>
2026-03-01 18:59:26 -0500

    server end set up, host, uid etc

---
+++ guandan/server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+
