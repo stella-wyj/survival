b456d6606ae448a89c76cf461ccc60928c512b07
stella-wyj <s.wangyj06@gmail.com>
2026-06-16 15:25:02 -0400

    changing routing system

--- guandan/server/index.js
+++ guandan/server/index.js
@@ -458,7 +458,7 @@ io.on('connection', (socket) => {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // 2. Redirect all web requests to your frontend index.html file
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
